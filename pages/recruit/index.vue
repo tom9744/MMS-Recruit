08bccbeeca03e1aad439abdfc398a79ca3d6d186
Junyoung Yang <tom9744@gmail.com>
2021-03-31 21:28:24 +0900

    feat: #3 'Recruit Page'의 마크업 및 텍스트 컨텐츠 입력 완료

--- pages/recruit/index.vue
+++ pages/recruit/index.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>Recruit Page</div>
+  <div class="container">
+    <section>
+      <h1>Recruit</h1>
+
+      <article>
+        <h2>앗...! 지금은 모집기간이 아닙니다.</h2>
+
+        <p>다음 학기 모집기간을 기다려 주시거나, 저희에게 직접 문의해주세요.</p>
+      </article>
+    </section>
+
+    <section>
+      <h1>지원 안내</h1>
+
+      <p>매주 화요일 6시, 정기 회의를 진행합니다.</p>
+
+      <article>
+        <h2>모집 일정</h2>
+
+        <h3>1학기</h3>
+        <p>O월 O일 23:59까지</p>
+
+        <h3>2학기</h3>
+        <p>O월 O일 23:59까지</p>
+      </article>
+
+      <article>
+        <h2>지원 자격</h2>
+
+        <ul>
+          <li>밴드 음악, 악기에 관심이 있는 누구나!</li>
+          <li>영상 제작, 믹싱에 관심있는 누구나!</li>
+        </ul>
+      </article>
+
+      <article>
+        <h2>모집 분야</h2>
+
+        <ul>
+          <li>밴드팀: 보컬/기타/베이스/드럼/키보드</li>
+          <li>영상팀: 영상 편집</li>
+          <li>음향팀: 녹음/프로듀싱</li>
+        </ul>
+
+        <p>로직, 파이널컷 가능자 우대</p>
+      </article>
+    </section>
+
+    <section>
+      <h1>지원하기</h1>
+
+      <p>매주 화요일 6시, 정기 회의를 진행합니다.</p>
+
+      <a href="https://forms.gle/14UzTR1D8xpXUi5p8">지원하기</a>
+    </section>
+  </div>
 </template>
 
 <script>
