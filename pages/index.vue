ed4a26b1b37247e999d61e7064b54a5fea680153
Junyoung Yang <tom9744@gmail.com>
2021-04-01 00:49:48 +0900

    feat: #3 'Main Page'의 CSS 스타일 초안

--- pages/index.vue
+++ pages/index.vue
@@ -1,180 +1,226 @@
 <template>
   <div class="container">
-    <section>
-      <h1>
+    <section class="carousel">
+      <h1 class="carousel--text">
         신새벽의 벅찬 가슴으로 민중의 새날을 노래하는 공과대 노래패, 목멱성
       </h1>
     </section>
 
-    <section>
-      <h2>About</h2>
+    <section class="main-section">
+      <article class="content">
+        <h2 class="content--title">About</h2>
 
-      <article>
-        <h3>목멱성은 어떤 동아리인가요?</h3>
-        <p>
-          목멱성은 악기를 배우고 싶거나 음악과 관련된 컨텐트에 관심이 있는
-          누구나 들어올 수 있는 동아리입니다! 🎤
-        </p>
+        <article class="content--body">
+          <h3 class="content--subtitle">🤔 목멱성은 어떤 동아리인가요?</h3>
+
+          <p class="content--paragraph">
+            목멱성은 밴드 음악 동아리로 시작했지만, 악기를 배우고 싶거나 음악과
+            관련된 컨텐츠에 관심이 있는 누구나 들어올 수 있는 동아리입니다.
+            악기를 직접 연주하지 않더라도 공연 영상 촬영/편집, 음원 프로듀싱 등
+            다양한 활동을 경험해볼 수 있는 종합 음악 컨텐츠 제작 동아리입니다!
+          </p>
+        </article>
+        <!-- Content Body -->
       </article>
+      <!-- Content -->
     </section>
 
-    <section>
-      <h2>Introduction</h2>
+    <section class="main-section">
+      <article class="content">
+        <h2 class="content--title">Introduction</h2>
 
-      <article>
-        <h3>다양한 악기 및 음향장비 보유</h3>
-        <p>
-          목멱성은 밴드 음악뿐만 아니라 다양한 장르의 음악을 포괄적으로 다루고
-          있습니다. 이에 필요한 다양한 악기와 음향 장비를 자체 보유하고
-          있습니다.
-        </p>
+        <article class="content--body">
+          <h3 class="content--subtitle">🎸 다양한 악기 및 음향장비 보유</h3>
 
-        <p>
-          또한, 영상 편집에 필요한 데스크탑과 각종 편집툴에 대한 라이센스를
-          보유하고 있습니다.
-        </p>
+          <p class="content--paragraph">
+            목멱성은 밴드 음악뿐만 아니라 다양한 장르의 음악과 영상 편집 등
+            다양한 분야를 포괄적으로 다루고 있습니다. 이에 필요한 다양한 악기와
+            음향 장비를 자체 보유하고 있습니다.
+          </p>
 
-        <ul>
-          <li>무슨무슨 악기 n개</li>
-          <li>무슨무슨 악기 n개</li>
-          <li>무슨무슨 장비 n개</li>
-          <li>무슨무슨 장비 n개</li>
-          <li>무슨무슨 장비 n개</li>
-        </ul>
+          <ul class="content--list instruments">
+            <li class="content--list-item instruments--icon">기타</li>
+            <li class="content--list-item instruments--icon">베이스</li>
+            <li class="content--list-item instruments--icon">드럼</li>
+            <li class="content--list-item instruments--icon">키보드</li>
+            <li class="content--list-item instruments--icon">아이맥</li>
+            <li class="content--list-item instruments--icon">앰프</li>
+          </ul>
+        </article>
+        <!-- Content Body -->
       </article>
+      <!-- Content -->
     </section>
 
-    <section>
-      <h2>Why MMS?</h2>
+    <section class="main-section">
+      <article class="content">
+        <h2 class="content--title">Why MMS?</h2>
 
-      <article>
-        <h3>거의 완벽에 가까운 음주/학업 병행</h3>
-        <p>
-          목멱성은 타의추종을 불허하는 술자리 문화를 꾸준히 가꿔오고 있습니다.
-          하지만 시험기간에는 동아리 내 스터디를 통해 전공 지식을 공유하는 등,
-          자율적으로 학업에 열중하는 분위기를 가지고 있습니다.
-        </p>
+        <article class="content--body">
+          <ul class="content--list advantage">
+            <li class="content--list-item advantage--card">
+              <h3 class="advantage--title">🍻 완벽에 가까운 음주/학업 병행</h3>
+
+              <p class="advantage--paragraph">
+                목멱성은 타의추종을 불허하는 술자리 문화를 꾸준히 가꿔오고
+                있습니다. 하지만 시험기간에는 동아리 내 스터디를 통해 전공
+                지식을 공유하는 등, 자율적으로 학업에 열중하는 분위기를 가지고
+                있습니다.
+              </p>
+            </li>
+
+            <li class="content--list-item advantage--card">
+              <h3 class="advantage--title">💞 활발한 선후배 커뮤니티</h3>
+
+              <p class="advantage--paragraph">
+                1993년 창립된 목멱성은 자유로운 선후배 문화를 토대로 30년 간
+                풍부한 OB 네트워크를 쌓아오고 있습니다. 사회의 다양한 분야에
+                진출하여 활동하고 계신 선배님들로부터 조언을 구할 수 있습니다.
+              </p>
+            </li>
+
+            <li class="content--list-item advantage--card">
+              <h3 class="advantage--title">👨‍👩‍👧‍👦 다양한 배경의 동아리 구성원</h3>
+
+              <p class="advantage--paragraph">
+                본래 공과대학 소모임으로 시작한 목멱성은 전자전기공학과와
+                컴퓨터공학과 학우들을 위주로 구성되어 왔습니다. 하지만 최근 중앙
+                동아리로 전환하며, 공과대학을 벗어나 더욱 다양한 학과의 학우들로
+                구성되게 되었습니다.
+              </p>
+            </li>
+          </ul>
+        </article>
+        <!-- Content Body -->
       </article>
-
-      <article>
-        <h3>활발한 선후배 커뮤니티</h3>
-
-        <p>
-          1993년 창립된 목멱성은 자유로운 선후배 문화를 토대로 30년 간 풍부한 OB
-          네트워크를 쌓아오고 있습니다. 사회의 다양한 분야에 진출하여 활동하고
-          계신 선배님들로부터 조언을 구할 수 있습니다.
-        </p>
-      </article>
-
-      <article>
-        <h3>다양한 배경의 동아리 구성원</h3>
-
-        <p>
-          본래 공과대학 소모임으로 시작한 목멱성은 전자전기공학과와 컴퓨터공학과
-          학우들을 위주로 구성되어 왔습니다. 하지만 최근 중앙 동아리로 전환하며,
-          공과대학을 벗어나 더욱 다양한 학과의 학우들로 구성되게 되었습니다.
-        </p>
-      </article>
+      <!-- Content -->
     </section>
 
-    <section>
-      <h2>Activity</h2>
+    <section class="main-section">
+      <article class="content">
+        <h2 class="content--title">Activity</h2>
 
-      <article>
-        <h3>목멱성은 어떤 활동을 하나요?</h3>
+        <article class="content--body">
+          <h3 class="content--subtitle">📆 목멱성은 어떤 활동을 하나요?</h3>
 
-        <div>
-          <h4>3월 - 학관 앞 공연</h4>
-          <p>학생회관 앞에서 대규모의 버스킹 공연을 진행합니다.</p>
-          <figure>
-            <img src="" alt="" />
-          </figure>
-        </div>
+          <p class="content--paragraph">
+            목멱성은 매주 화요일 6시 정기회의를 진행하며, 연간 2회의 공연을
+            진행합니다. 그 외에도 MT, 체육대회, 동아리 내 소모임 등 다양한
+            활동을 진행하고 있습니다.
+          </p>
 
-        <div>
-          <h4>4월 - MT</h4>
-          <p>선후배, OB까지 함께하는 MT를 진행합니다.</p>
-          <figure>
-            <img src="" alt="" />
-          </figure>
-        </div>
+          <div>
+            <h4>3월 - 학관 앞 공연</h4>
+            <p>학생회관 앞에서 대규모의 버스킹 공연을 진행합니다.</p>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+          </div>
 
-        <div>
-          <h4>상시 - 소규모 소풍</h4>
-          <p>마음맞는 친구들과 한강공원 등으로 소풍을 갑니다.</p>
-          <figure>
-            <img src="" alt="" />
-          </figure>
-        </div>
+          <div>
+            <h4>4월 - MT</h4>
+            <p>선후배, OB까지 함께하는 MT를 진행합니다.</p>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+          </div>
 
-        <div>
-          <h4>5월 - 정기공연</h4>
-          <p>목멱성의 1년 행사 중 가장 중요한 정기공연입니다.</p>
-          <figure>
-            <img src="" alt="" />
-          </figure>
-        </div>
+          <div>
+            <h4>상시 - 소규모 소풍</h4>
+            <p>마음맞는 친구들과 한강공원 등으로 소풍을 갑니다.</p>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+          </div>
 
-        <div>
-          <h4>9월 - 창립제 / 체육대회</h4>
-          <p>목멱성 창립일을 기념하며, 체육대회를 진행합니다.</p>
-          <figure>
-            <img src="" alt="" />
-          </figure>
-        </div>
+          <div>
+            <h4>5월 - 정기공연</h4>
+            <p>목멱성의 1년 행사 중 가장 중요한 정기공연입니다.</p>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+          </div>
 
-        <div>
-          <h4>상시 - 외부공연</h4>
-          <p>홍대/신촌 인근 클럽을 대관하여, 외부공연을 진행합니다.</p>
-          <figure>
-            <img src="" alt="" />
-          </figure>
-        </div>
+          <div>
+            <h4>9월 - 창립제 / 체육대회</h4>
+            <p>목멱성 창립일을 기념하며, 체육대회를 진행합니다.</p>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+          </div>
+
+          <div>
+            <h4>상시 - 외부공연</h4>
+            <p>홍대/신촌 인근 클럽을 대관하여, 외부공연을 진행합니다.</p>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+          </div>
+
+          <div>
+            <h3>상시 - 족멱성</h3>
+            <p>풋살, 족구 등 구기종목을 사랑하는 사람들의 소모임입니다.</p>
+            <figure>
+              <img src="" alt="" />
+            </figure>
+          </div>
+        </article>
+        <!-- Content Body -->
       </article>
-
-      <article>
-        <h3>상시 - 족멱성</h3>
-        <p>풋살, 족구 등 구기종목을 사랑하는 사람들의 소모임입니다.</p>
-        <figure>
-          <img src="" alt="" />
-        </figure>
-      </article>
+      <!-- Content -->
     </section>
 
-    <section>
-      <h2>Project</h2>
+    <section class="main-section">
+      <article class="content">
+        <h2 class="content--title">Project</h2>
 
-      <article>
-        <h3>목멱성 활동 결과물을 확인해보세요!</h3>
-        <p>
-          목멱성은 매년 학관 앞 공연, 정기공연, 상시 외부공연을 통해 음악 활동을
-          계속하고 있습니다. 또한 공연 실황을 촬영 및 편집하여 YouTube 등 채널에
-          업로드하고 있습니다.
-        </p>
+        <article class="content--body">
+          <h3 class="content--subtitle">
+            🔥 목멱성 활동 결과물을 확인해보세요!
+          </h3>
+          <p class="content--paragraph">
+            목멱성은 매년 학관 앞 공연, 정기공연, 상시 외부공연을 통해 음악
+            활동을 계속하고 있습니다. 또한 공연 실황을 촬영 및 편집하여 YouTube
+            등 채널에 업로드하고 있습니다.
+          </p>
 
-        <div>
-          <video src=""></video>
-        </div>
+          <iframe
+            class="content--video"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/VoRaLXE5Dzw"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </article>
+        <!-- Content Body -->
       </article>
+      <!-- Content -->
     </section>
 
-    <section>
-      <h2>Contact</h2>
+    <section class="main-section">
+      <article class="content">
+        <h2 class="content--title">Contact</h2>
 
-      <article>
-        <h3>목멱성과 함께 하고 싶으신가요?</h3>
-        <p>
-          목멱성 매학기 시작과 함께(3월, 9월) 신규 모집을 진행합니다. 하지만
-          학기 중에 전혀 신규 모집을 진행하지 않는 것은 아닙니다. 목멱성과
-          함께하고 싶으시면, 아래의 채널을 통해 문의해주세요!
-        </p>
+        <article class="content--body">
+          <h3 class="content--subtitle">🤗 목멱성과 함께 하고 싶으신가요?</h3>
+          <p class="content--paragraph">
+            목멱성 매학기 시작과 함께(3월, 9월) 신규 모집을 진행합니다. 하지만
+            학기 중에 전혀 신규 모집을 진행하지 않는 것은 아닙니다. 목멱성과
+            함께하고 싶으시면, 아래의 채널을 통해 문의해주세요!
+          </p>
+
+          <ul class="content--list contact">
+            <li class="content--list-item contact--icon">Facebook</li>
+            <li class="content--list-item contact--icon">Instagram</li>
+            <li class="content--list-item contact--icon">Kakao Talk</li>
+          </ul>
+        </article>
+        <!-- Content Body -->
       </article>
-
-      <ul>
-        <li>FB</li>
-        <li>Insta</li>
-        <li>Kakao</li>
-      </ul>
+      <!-- Content -->
     </section>
   </div>
 </template>
@@ -183,4 +229,220 @@
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.carousel {
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: gainsboro;
+
+  &--text {
+    padding: 0 20%;
+  }
+}
+
+.main-section {
+  width: 100vw;
+
+  padding: 30% 5%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:nth-child(odd) {
+    background-color: #f3f3f3;
+  }
+}
+
+.content {
+  padding: 8px;
+
+  &--title {
+    margin-bottom: 2rem;
+
+    text-shadow: 2px 2px lightgray;
+  }
+
+  &--body {
+    padding: 8px;
+  }
+
+  &--subtitle {
+    margin-bottom: 1rem;
+  }
+
+  &--paragraph {
+    margin-bottom: 1rem;
+  }
+
+  &--video {
+    height: 50vw;
+    max-height: 720px;
+    width: 100%;
+  }
+
+  &--list {
+    list-style: none;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    padding: 0;
+  }
+
+  &--list-item {
+  }
+}
+
+.instruments {
+  justify-content: center;
+
+  gap: 16px;
+  padding: 16px 0px;
+
+  &--icon {
+    margin: 0.5rem;
+    padding: 0.5rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 100px;
+    width: 100px;
+
+    background-color: white;
+
+    border-radius: 50%;
+  }
+}
+
+.advantage {
+  &--card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    padding: 10%;
+  }
+
+  &--title {
+    margin-bottom: 1rem;
+  }
+
+  &--paragraph {
+  }
+}
+
+.contact {
+  flex-wrap: nowrap;
+  justify-content: space-around;
+
+  padding: 16px 0px;
+
+  &--icon {
+    margin: 0.5rem;
+    padding: 0.5rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    text-align: center;
+
+    height: 55px;
+    width: 55px;
+
+    background-color: white;
+
+    border-radius: 50%;
+  }
+}
+
+@media screen and (min-width: 400px) {
+  .carousel {
+    height: 90vh;
+  }
+
+  .main-section {
+    padding: 25% 15%;
+  }
+
+  .content {
+    &--video {
+      height: 40vw;
+    }
+  }
+
+  .contact {
+    &--icon {
+      height: 70px;
+      width: 70px;
+    }
+  }
+}
+
+@media screen and (min-width: 780px) {
+  .carousel {
+    height: 80vh;
+  }
+
+  .main-section {
+    padding: 20% 20%;
+  }
+
+  .content {
+    &--video {
+      height: 35vw;
+    }
+  }
+
+  .advantage {
+    flex-wrap: nowrap;
+
+    align-items: baseline;
+
+    &--card {
+      padding: 2.5%;
+    }
+  }
+
+  .contact {
+    &--icon {
+      height: 85px;
+      width: 85px;
+    }
+  }
+}
+
+@media screen and (min-width: 1080px) {
+  .carousel {
+    height: 70vh;
+  }
+
+  .main-section {
+    padding: 15% 20%;
+  }
+
+  .content {
+    &--video {
+      height: 30vw;
+    }
+  }
+
+  .contact {
+    &--icon {
+      height: 100px;
+      width: 100px;
+    }
+  }
+}
+</style>
