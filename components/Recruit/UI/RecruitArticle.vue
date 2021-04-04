b80885d290955fc8fc1df4aee1ba45a69d3ac443
Junyoung Yang <tom9744@gmail.com>
2021-04-04 15:02:01 +0900

    fix: #1 'Recruit Page' 컴포넌트 분리

---
+++ components/Recruit/UI/RecruitArticle.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="recruit-article">
+    <h2 class="recruit-article--title"><slot name="title" /></h2>
+
+    <div class="recruit-article--content">
+      <slot name="content" />
+    </div>
+  </article>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.recruit-article {
+  width: 100%;
+
+  padding: 5% 0;
+
+  &--title {
+    margin-bottom: 2rem;
+  }
+}
+</style>
